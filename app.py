2afdef665109e5c84021db552b57aeab78696df3
arco8272 <105204411+arco8272@users.noreply.github.com>
2025-08-19 22:25:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask ,jsonify,render_template
 from jwttoken import jwtretttoap
 import os
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='\')
 
 @app.route('/')
 def serve_html():
@@ -27,6 +27,7 @@ def jwtjson(pas,userid):
         return data
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
 
